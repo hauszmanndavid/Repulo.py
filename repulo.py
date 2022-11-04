d94a94fd84388fcb2cce48c389afff8bd0efdf1a
hauszmanndavid <hauszmannd@gmail.com>
2022-11-04 16:05:43 +0100

    D resz done

--- repulo.py
+++ repulo.py
@@ -12,3 +12,18 @@ print("A mérési eredmények:", ' '.join(map(str,Llista)))
 print("Ez a legnagyobb hely: ", max(Llista))
 
 #C rész
+
+#D rész
+viz = 0
+for i in Llista:
+    if i == 0:
+        viz += 1
+
+szárazfold = 20 - viz
+
+if szárazfold > 10:
+    print("A szárazföld több")
+elif viz > 10:
+    print("A víz több")
+else:
+    print("Mind a kettőből ugyanannyi van")
